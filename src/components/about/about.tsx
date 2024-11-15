12df87d01e817bc5c8fc1043ecd909852a3b1731
Lucas Mendes <45476652+lucasfabbrim@users.noreply.github.com>
2024-11-15 15:27:37 -0300

    Update about.tsx

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -2,19 +2,19 @@ export default function About() {
   return (
     <section
       id="about"
-      className="relative w-full h-auto overflow-hidden mb-96 pt-20"
+      className="mx-auto max-w-7xl p-8 py-16 md:py-24 lg:px-8 mb-96"
     >
-      <div className="flex flex-col text-start items-start px-6">
-        <h1 className="leading-none bg-clip-text text-transparent bg-gradient-to-br from-white via-zinc-400 to-zinc-800 text-3xl font-bold max-w-72 border-b border-b-zinc-800 pb-5">
+      <div className="grid items-start justify-between gap-5 md:grid-cols-2">
+        <h1 className="leading-none bg-clip-text text-transparent bg-gradient-to-br from-white via-zinc-400 to-zinc-800 pr-8 text-3xl font-bold tracking-tight md:pr-16 md:text-5xl/[1.07]">
           Quem disse que organização tem que ser chato?
         </h1>
-        <h3 className="pt-5 text-zinc-400 text-wrap max-w-xs text-sm font-normal tracking-normal leading-tight">
-          Com o <span className="text-zinc-300 font-bold">Note Private</span>,
+        <h3 className="pt-5 text-base text-zinc-400/80 font-medium">
+          Com o <span className="text-zinc-300 font-medium">Note Private</span>,
           gerenciar a organização da sua vida é fácil, fortalecedor e nada
           chato. Nosso template intuitiva traz clareza, simplifica sua tomada de
           decisões financeiras e coloca o poder da organização na ponta dos seus
           dedos.
-          <span className="text-zinc-300 font-bold">
+          <span className="text-zinc-300 font-medium">
             {" "}
             Diga não às planilhas e ferramentas projetadas nos anos 80.
           </span>
