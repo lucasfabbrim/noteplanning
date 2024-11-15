34d1e70a41b366872be0d7c61c8731b93c98bda7
Lucas Mendes <45476652+lucasfabbrim@users.noreply.github.com>
2024-11-15 14:10:53 -0300

    Update subtitle.tsx

--- src/components/hero-section/subtitle.tsx
+++ src/components/hero-section/subtitle.tsx
@@ -1,6 +1,6 @@
 export function SubTitle() {
   return (
-    <h3 className="text-neutral-400 pt-3 font-normal  text-xs">
+    <h3 className="text-neutral-400 pt-3 font-normal text-sm">
       Seu novo planejamento totalmente personalizado está a um clique de
       distância.
       <span className="text-neutral-300 font-medium"> Comece agora.</span>
