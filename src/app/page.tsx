bcaceda3c69c23cb13c097654e878bfc976a5d11
Lucas Mendes <45476652+lucasfabbrim@users.noreply.github.com>
2024-11-19 12:16:09 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Entreteniment from "@/components/entreteniment/entreteniment";
 import Footer from "@/components/footer/footer";
 import Hero from "@/components/hero-section/hero";
 import Intro from "@/components/intro/intro";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function Home() {
   return (
     <div className="h-screen bg-black font-inter">
+      <GoogleAnalytics gaId="G-Q7L1FTGB2K" />
       <main className="flex flex-col">
         <Hero />
         <Intro />
