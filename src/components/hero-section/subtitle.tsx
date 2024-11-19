c2212fb7712f966f7445464ec779c8f559fe8736
Lucas Mendes <45476652+lucasfabbrim@users.noreply.github.com>
2024-11-19 17:32:00 -0300

    Update subtitle.tsx

--- src/components/hero-section/subtitle.tsx
+++ src/components/hero-section/subtitle.tsx
@@ -1,6 +1,6 @@
 export function SubTitle() {
   return (
-    <h3 className="relative leading-none text-zinc-300/90 pt-2 font-light max-w-[350px] text-sm">
+    <h3 className="relative leading-none text-zinc-300/90 pt-2 font-normal max-w-[350px] text-sm">
       Seu novo planejamento totalmente personalizado está a um clique de
       distância.
     </h3>
