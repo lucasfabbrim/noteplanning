a9001748110356ece35469e2b79a7e7280c9aaa1
Lucas Mendes <45476652+lucasfabbrim@users.noreply.github.com>
2024-11-22 11:02:57 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <GoogleAnalytics gaId="G-FXZJ4HXH96" />
 
       <div
-        className="absolute top-10 right-60 lg:top-0 lg:left-0  lg:w-[600px] w-[300px] h-[230px] bg-sky-500 blur[300px] opacity-20"
+        className="absolute top-10 right-60 lg:top-0 lg:left-0  lg:w-[600px] w-[300px] h-[230px] bg-zinc-200 blur[300px] opacity-20"
         style={{
           filter: "blur(170px)",
         }}
@@ -27,17 +27,16 @@ export default function Home() {
             <div className="flex flex-col">
               <h1 className="text-white text-3xl font-bold ">
                 Organize-se e produza{" "}
-                <span className="bg-gradient-to-l from-blue-700 to-cyan-300 text-transparent bg-clip-text">
+                <span className="bg-gradient-to-l from-zinc-300 to-zinc-500 text-transparent bg-clip-text">
                   mais
                 </span>
-                .
               </h1>
               <h3 className="text-zinc-300/90 text-base text-center pt-3 font-medium">
                 Seu novo planejamento totalmente personalizado está a um clique
                 de distância.
               </h3>
               <div className="flex justify-center items-center pt-6">
-                <Button className="bg-blue-600/10 border border-cyan-600 w-auto h-14 rounded-full text-lg text-white">
+                <Button className="bg-transparent border border-zinc-700 w-auto h-14 rounded-full text-lg text-white">
                   <span className="px-4 flex items-center gap-2">
                     Quero organizar minha vida
                     <ChevronRight size={12} strokeWidth={3} />
@@ -58,10 +57,9 @@ export default function Home() {
           </div>
           <div className="text-2xl text-white pt-16 font-semibold px-2">
             Faça como empresas líderes do mercado organize usando o{" "}
-            <span className="bg-gradient-to-l from-blue-700 to-cyan-300 text-transparent bg-clip-text">
+            <span className="bg-gradient-to-l from-zinc-300 to-zinc-500 text-transparent bg-clip-text">
               Notion
             </span>
-            .
           </div>
           <div className="text-sm text-zinc-400/90 font-medium px-4 pt-4">
             O nosso template facilita o controle da sua produtividade e
